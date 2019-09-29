00e5d71f263b63698d324172c30fd681b0105467
Konstantin Bychkov <k.bychkov@yandex.ru>
2019-09-29 12:23:43 +0700

    feat(client): add Google Analitycs tracking

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import ReactGA from 'react-ga';
 import './App.css';
 import Header from './Header';
 import Crawler from './Crawler';
 
 export default class App extends Component {
+  componentDidMount() {
+    if (process.env.REACT_APP_TRACKING_ID) {
+      ReactGA.initialize(process.env.REACT_APP_TRACKING_ID);
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+  }
+
   render() {
     return (
       <div className="App">
