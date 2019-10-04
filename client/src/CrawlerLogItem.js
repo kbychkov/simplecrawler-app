4596c0df3af16f29b668f5f6e81f3b53bf332f54
Konstantin Bychkov <k.bychkov@yandex.ru>
2019-10-04 21:07:34 +0700

    feat(client): show queue item data

--- client/src/CrawlerLogItem.js
+++ client/src/CrawlerLogItem.js
@@ -5,6 +5,10 @@ export default class CrawlerLogItem extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      isOpen: false
+    };
+
     this.crawlerLogItemModifier = '';
     const { code } = props.data.stateData;
 
@@ -17,17 +21,46 @@ export default class CrawlerLogItem extends Component {
     } else if (code >= 500) {
       this.crawlerLogItemModifier = 'CrawlerLogItem-code--5xx';
     }
+
+    this.toggleItem = this.toggleItem.bind(this);
+  }
+
+  toggleItem() {
+    this.setState(state => ({
+      isOpen: !state.isOpen
+    }));
+  }
+
+  getItemModifier() {
+    return this.state.isOpen ? 'CrawlerLogItem--open' : '';
   }
 
   render() {
+    const headers = this.props.data.stateData.headers;
+    const headersItems = Object.keys(headers).map(key => (
+      <li key={key}><span>{key}:</span> {headers[key]}</li>
+    ));
+
     return (
-      <div className="CrawlerLogItem">
-        <div className="CrawlerLogItem-url">
+      <div className={'CrawlerLogItem ' + this.getItemModifier()}>
+        <div className="CrawlerLogItem-url" onClick={this.toggleItem}>
           {this.props.data.url}
         </div>
         <div className={'CrawlerLogItem-code ' + this.crawlerLogItemModifier}>
           {this.props.data.stateData.code}
         </div>
+        <dl className="CrawlerLogItem-details">
+          <dt className="CrawlerLogItem-detailsTitle">depth</dt>
+          <dd className="CrawlerLogItem-detailsData">{this.props.data.depth}</dd>
+          <dt className="CrawlerLogItem-detailsTitle">referrer</dt>
+          <dd className="CrawlerLogItem-detailsData">{this.props.data.referrer}</dd>
+          <dt className="CrawlerLogItem-detailsTitle">headers</dt>
+          <dd className="CrawlerLogItem-detailsData">
+            <ul className="CrawlerLogItem-headers">
+              {headersItems}
+            </ul>
+          </dd>
+        </dl>
       </div>
     );
   }
