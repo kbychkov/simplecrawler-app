333e58b0bb33c18791068191186b46c839a2fce0
Konstantin Bychkov <k.bychkov@yandex.ru>
2019-10-26 19:24:40 +0700

    fix(server): limit max number of URLs per crawler

--- server/services/simplecrawler/simplecrawler.class.js
+++ server/services/simplecrawler/simplecrawler.class.js
@@ -29,7 +29,7 @@ class Service {
 
     crawler.addFetchCondition((queueItem, referrerQueueItem, callback) => {
       crawler.queue.getLength((err, length) => {
-        callback(null, !(length >= limit));
+        callback(null, !(length >= (this.app.get('crawlerLimit') || limit)));
       });
     });
 
